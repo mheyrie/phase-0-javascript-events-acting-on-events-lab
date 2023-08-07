2d9bf83c2626239f7aa90989be2baa19d2918546
Mary <omota4fidelity@gmail.com>
2023-08-07 10:49:12 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left.replace('px', ''));
+    const newLeft = left + 1;
+    dodger.style.left = `${newLeft}px`;
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
